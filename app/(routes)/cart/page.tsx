c83f7074c9f1fcc820713dbc8ea7589b96235e07
Niall Moore <niallam22@gmail.com>
2023-11-03 18:57:14 +0000

    revert changes

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,6 +1,6 @@
-// "use client";
+"use client"
 
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Container from '@/components/ui/container';
 import useCart from '@/hooks/use-cart';
@@ -11,16 +11,16 @@ import CartItem from './components/cart-item';
 export const revalidate = 0;
 
 const CartPage = () => {
-  // const [isMounted, setIsMounted] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
   const cart = useCart();
 
-  // useEffect(() => {
-  //   setIsMounted(true);
-  // }, []);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-  // if (!isMounted) {
-  //   return null;
-  // }
+  if (!isMounted) {
+    return null;
+  }
 
   return (
     <div className="bg-white">
