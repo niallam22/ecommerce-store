a8fc15eaa14111cfd184d0957be4f016092f8499
Niall Moore <niallam22@gmail.com>
2023-11-20 07:35:00 +0000

    order summary

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -9,13 +9,13 @@ import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
 import { toast } from "react-hot-toast";
 
-interface SummaryProps{
-  isError: boolean
+interface SummaryProps {
+  isError: boolean;
 }
 
-const Summary: React.FC<SummaryProps> = ({isError}) => {
+const Summary: React.FC<SummaryProps> = ({ isError }) => {
   const searchParams = useSearchParams();
-  const items = useCart((state) => state.items); //selector function passed to zustand lib through useCart and returns result
+  const items = useCart((state) => state.items);
   const removeAll = useCart((state) => state.removeAll);
 
   useEffect(() => {
@@ -29,36 +29,76 @@ const Summary: React.FC<SummaryProps> = ({isError}) => {
     }
   }, [searchParams, removeAll]);
 
-  const totalPrice = items.reduce((total, item) => {
-    return total + Number(item.price)
+  const shippingCost = 3.25;
+  const itemsTotal = items.reduce((total, item) => {
+    return total + Number(item.price) * item.quantity;
   }, 0);
+  const subtotal = itemsTotal + shippingCost
+  const vat = subtotal * 0.2;
+  const orderTotal = subtotal + vat;
 
   const onCheckout = async () => {
     const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
-      productIds: items.map((item) => item.id)
+      productIds: items.map((item) => item.id),
     });
 
     window.location = response.data.url;
-  }
+  };
 
-  return ( 
-    <div
-      className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
-    >
-      <h2 className="text-lg font-medium text-gray-900">
-        Order summary
-      </h2>
+  return (
+    <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
+      <h2 className="text-lg font-bold text-gray-900">Order summary</h2>
       <div className="mt-6 space-y-4">
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
-          <div className="text-base font-medium text-gray-900">Order total</div>
-         <Currency value={totalPrice} />
+          <div className="text-base font-medium text-gray-900">Item total</div>
+          <Currency value={itemsTotal} />
+        </div>
+        <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+          <div className="text-base font-medium text-gray-600">Shipping</div>
+          <Currency value={shippingCost} />
+        </div>
+        <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+          <div className="text-base font-medium text-gray-600">VAT (20%)</div>
+          <Currency value={vat} />
+        </div>
+        <div className="flex items-center justify-between border-t border-black pt-4">
+          <div className="text-base text-gray-600 font-bold">Order total</div>
+          <Currency value={orderTotal} />
         </div>
       </div>
-      <Button onClick={onCheckout} disabled={items.length === 0|| isError} className="w-full mt-6">
+      <Button onClick={onCheckout} disabled={items.length === 0 || isError} className="w-full mt-6">
         Checkout
       </Button>
     </div>
   );
-}
- 
+  
+  // return (
+  //   <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
+  //     <h2 className="text-lg font-medium text-gray-900">Order summary</h2>
+  //     <div className="mt-6 space-y-4">
+  //       <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+  //         <div className="text-base font-medium text-gray-900">Item total</div>
+  //         <Currency value={itemsTotal} />
+  //       </div>
+  //       <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+  //         <div className="text-base font-medium text-gray-900">Shipping</div>
+  //         <Currency value={shippingCost} />
+  //       </div>
+  //       <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+  //         <div className="text-base font-medium text-gray-900">VAT (20%)</div>
+  //         <Currency value={vat} />
+  //       </div>
+  //       <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+  //         <div className="text-base font-medium text-gray-900">Order total</div>
+  //         <Currency value={orderTotal} />
+  //       </div>
+  //     </div>
+  //     <Button onClick={onCheckout} disabled={items.length === 0 || isError} className="w-full mt-6">
+  //       Checkout
+  //     </Button>
+  //   </div>
+  // );
+};
+
 export default Summary;
+
