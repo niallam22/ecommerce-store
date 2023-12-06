a46938058a6ecc17ff673632bf3348e9a3bbe803
Niall Moore <niallam22@gmail.com>
2023-12-06 10:31:50 +0000

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,23 @@
+import Link from "next/link";
+import { Instagram } from 'lucide-react';
+
 const Footer = () => {
   return (
     <footer className="bg-white border-t">
-      <div className="mx-auto py-10">
+      <div className="mx-auto py-8">
+        <div className="m-auto w-fit">
+          <div className="flex justify-between items-center pb-2">
+            <Instagram />
+            <Link className="text-sm hover:decoration-solid" href="/about">About</Link>
+            <Link className="text-sm hover:decoration-solid" href="/contact">Contact</Link>
+            <Link className="text-sm hover:decoration-solid" href="/returns">Returns</Link>
+          </div>
+
         <p className="text-center text-xs text-black">
-          &copy; 2023 Store, Inc. All rights reserved.
+          &copy; 2023 Lazy Bones Ltd. All rights reserved.
         </p>
+        </div>
+
       </div>
     </footer>
   )
