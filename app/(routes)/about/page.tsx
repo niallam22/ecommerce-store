68523566cb9b5b909a00c20ba3ddc1b30e870926
Niall Moore <niallam22@gmail.com>
2023-12-06 13:24:04 +0000

    handle errors

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -25,7 +25,7 @@ const AboutPage = async () => {
           <div className="flex flex-col justify-center gap-y-4 md:grid md:grid-cols-2 pt-2">
             <img className="" src="http://placekitten.com/g/500/500" alt="founder" />
             <div className="p-4 h-fit my-auto">
-              <h4 className="text-neutral-500 text-sm">"The Labrador"</h4>
+            <h4 className="text-neutral-500 text-sm">&ldquo;The Labrador&rdquo;</h4>
               <h3 className="text-3xl font-bold text-black">Nathan Appleby</h3>
               <p className="text-neutral-500">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis iste sunt soluta illum, reprehenderit modi facilis minima vero incidunt ad perspiciatis voluptatum amet sed quibusdam dolorem ducimus veritatis? Rerum, eos.
@@ -33,7 +33,7 @@ const AboutPage = async () => {
             </div>
             <img className="md:order-last" src="http://placekitten.com/g/500/500" alt="founder" />
             <div className="p-4 h-fit my-auto">
-              <h4 className="text-neutral-500 text-sm">"The Collie"</h4>
+              <h4 className="text-neutral-500 text-sm">&ldquo;The Collie&ldquo;</h4>
               <h3 className="text-3xl font-bold text-black">Niall Moore</h3>
               <p className="text-neutral-500">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis iste sunt soluta illum, reprehenderit modi facilis minima vero incidunt ad perspiciatis voluptatum amet sed quibusdam dolorem ducimus veritatis? Rerum, eos.
